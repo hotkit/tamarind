c889e9832605f78c937f60016c80fcf69d093de0
Kirit Sælensminde <kirit@kirit.com>
2019-12-10 18:07:46 +0700

    Refactor project layout

--- tamarind/main.cpp
+++ tamarind/main.cpp
@@ -8,7 +8,6 @@ FSL_MAIN("tamarind", "Tamarind runtime")
 (fostlib::ostream &out, fostlib::arguments &args) {
     f5::tamarind::runtime rt;
     for (auto const &arg : args) {
-        out << arg << std::endl;
         rt.load(fostlib::coerce<fostlib::fs::path>(arg));
     }
     return rt.completion().get();
