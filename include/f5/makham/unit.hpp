6222234e2b48589a5255b57cfaa6ac962d931a9c
Kirit Sælensminde <kirit@kirit.com>
2019-12-11 10:12:48 +0700

    Start to add the coro library, Makham

---
+++ include/f5/makham/unit.hpp
@@ -0,0 +1,23 @@
+/**
+    Copyright 2019 Red Anchor Trading Co. Ltd.
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+ */
+
+
+#pragma once
+
+
+namespace f5::makham {
+
+
+    /// ## Work unit
+    /**
+     * Combines multiple tasks into a single awaitable that triggers when
+     * everything is done.
+     */
+    class unit {};
+
+
+}
