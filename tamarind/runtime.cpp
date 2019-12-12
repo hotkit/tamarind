ebf8b9862c101faa8d7b820b59185eec4e36f0a6
Kirit Sælensminde <kirit@kirit.com>
2019-12-12 12:08:05 +0700

    Create workflow objects.

--- tamarind/runtime.cpp
+++ tamarind/runtime.cpp
@@ -15,7 +15,8 @@
 
 struct f5::tamarind::runtime::impl {
     /// Store the workflows
-    f5::threading::tsmap<f5::u8shared, makham::task<workflow>> workflows;
+    f5::threading::tsmap<f5::u8string, makham::task<workflow>> workflows;
+    std::vector<std::experimental::coroutine_handle<>> resumables;
 };
 
 
@@ -38,5 +39,9 @@ namespace {
 void f5::tamarind::runtime::load(fostlib::fs::path fn) {
     f5::makham::post([this, fn = std::move(fn)]() {
         auto [name, ast] = f5::tamarind::parse::workflow(fn);
+        pimpl->workflows.add_if_not_found(name, [ast = std::move(ast)]() {
+            auto t = wf(std::move(ast));
+            return t;
+        });
     });
 }
---
+++ tamarind/workflow.cpp
@@ -0,0 +1,12 @@
+/**
+    Copyright 2019 Red Anchor Trading Co. Ltd.
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+ */
+
+
+#include <f5/tamarind/workflow.hpp>
+
+
+f5::tamarind::workflow::workflow(fostlib::json ast) {}
