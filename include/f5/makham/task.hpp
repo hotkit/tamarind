0739de1615145ca7829a94fde550bb53e1f24884
Kirit Sælensminde <kirit@kirit.com>
2019-12-16 06:52:57 +0700

    Add in logic for tying tasks together.

--- include/f5/makham/task.hpp
+++ include/f5/makham/task.hpp
@@ -27,18 +27,46 @@ namespace f5::makham {
     template<typename R>
     class task {
       public:
+        /// Not copyable
+        task(task const &) = delete;
+        task &operator=(task const &) = delete;
+        /// Movable
+        task(task &&t) : coro(t.coro) { t.coro = {}; }
+        task &operator=(task &&t) { swap(coro, t.coro); }
+
         struct promise_type {
+            std::exception_ptr exception;
             std::optional<R> value;
             std::atomic<bool> started = false;
             std::atomic<std::experimental::coroutine_handle<>> cb;
             std::promise<R> fp;
 
             void signal(std::experimental::coroutine_handle<> s) {
+                std::cout << "Setting callback " << this << std::endl;
                 auto old = cb.exchange(s);
                 if (old) {
                     throw std::invalid_argument{
                             "A task can only have one awaitable"};
                 }
+                if (s && (value.has_value() || exception)) {
+                    std::cout << "Executing cb because setting up signal"
+                              << std::endl;
+                    callback();
+                }
+            }
+            void callback() {
+                f5::makham::post([this]() {
+                    std::cout << "Starting callback" << std::endl;
+                    try {
+                        auto h = cb.exchange({});
+                        if (h) {
+                            std::cout << "Callback from " << this << std::endl;
+                            h.resume();
+                        }
+                    } catch (...) {
+                        std::cerr << "Exception exited cb.resume()\n";
+                    }
+                });
             }
 
             auto get_return_object() {
@@ -51,12 +79,11 @@ namespace f5::makham {
             auto return_value(R v) {
                 fp.set_value(v);
                 value = std::move(v);
+                callback();
                 return std::experimental::suspend_never{};
             }
             auto final_suspend() { return std::experimental::suspend_always{}; }
-            void unhandled_exception() {
-                std::cerr << "unhandled_exception in a task\n";
-            }
+            void unhandled_exception() { exception = std::current_exception(); }
         };
 
         std::experimental::coroutine_handle<> handle() const { return coro; }
@@ -64,7 +91,18 @@ namespace f5::makham {
         void start_async() {
             auto const running = coro.promise().started.exchange(true);
             if (not running) {
-                f5::makham::post([this]() { coro.resume(); });
+                std::cout << "Starting async" << std::endl;
+                f5::makham::post([this]() {
+                    std::cout << "Resuming " << &coro.promise() << std::endl;
+                    try {
+                        coro.resume();
+                    } catch (...) {
+                        std::cerr << "Exception exited coro.resume()"
+                                  << std::endl;
+                    }
+                });
+            } else {
+                std::cout << "Task already running" << std::endl;
             }
         }
 
@@ -79,7 +117,16 @@ namespace f5::makham {
             coro.promise().signal(awaiting);
             start_async();
         }
-        auto await_resume() { return *coro.promise().value; }
+        auto await_resume() {
+            auto &p = coro.promise();
+            if (p.exception) {
+                std::rethrow_exception(p.exception);
+            } else if (not p.value.has_value()) {
+                throw std::runtime_error("The coroutine doesn't have a value");
+            } else {
+                return *coro.promise().value;
+            }
+        }
 
       private:
         using handle_type = std::experimental::coroutine_handle<promise_type>;
